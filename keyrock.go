d935aee4d13abb9d219472fb438bc0a4c90a0857
Samuel T Jackson <14303082+SamuelTJackson@users.noreply.github.com>
2020-06-16 19:18:17 +0200

    .

--- keyrock.go
+++ keyrock.go
@@ -3,7 +3,9 @@ package keyrock
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"sync"
 )
 
 type Options struct {
@@ -16,21 +18,38 @@ type client struct {
 	httpClient *http.Client
 	options *Options
 	token string
+	mutex *sync.Mutex
+}
+func validateOptions(options *Options) error {
+	if len(options.password) == 0 {
+		return fmt.Errorf("password can not be empty")
+	}
+	if len(options.email) == 0{
+		return fmt.Errorf("email can not be empty")
+	}
+	if options.baseURL[len(options.baseURL) - 1:] == "/" {
+		options.baseURL = options.baseURL[:len(options.baseURL) - 1]
+	}
+	return nil
 }
 
 func NewClient(options *Options) (*client,error) {
+	if err := validateOptions(options); err != nil {
+		return nil, err
+	}
 	httpClient := &http.Client{}
 	newClient := &client{
 		httpClient: httpClient,
 		options: options,
+		mutex: &sync.Mutex{},
 	}
-	if err := newClient.getToken(); err != nil {
+	if err := newClient.refreshToken(); err != nil {
 		return nil, err
 	}
 	return newClient, nil
 }
 
-func (c *client) getToken() error {
+func (c *client) refreshToken() error {
 	body, err := json.Marshal(&user{
 		Name:     c.options.email,
 		Password: c.options.password,
@@ -44,6 +63,14 @@ func (c *client) getToken() error {
 		return err
 	}
 	defer resp.Body.Close()
+	c.mutex.Lock()
 	c.token = resp.Header.Get("X-Subject-Token")
+	c.mutex.Unlock()
 	return nil
+}
+
+func (c *client) getToken() string {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.token
 }
