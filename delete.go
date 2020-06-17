fb9b076ded337338b0f7a8c58c6a9fe14ca3e70a
Samuel T Jackson <14303082+SamuelTJackson@users.noreply.github.com>
2020-06-17 23:10:37 +0200

    .

--- delete.go
+++ delete.go
@@ -27,3 +27,25 @@ func (c client) DeleteApplication(id ID) error {
 	}
 	return nil
 }
+
+func (c *client) DeleteToken() error {
+	if err := c.validateToken(); err != nil {
+		return err
+	}
+	req, err := http.NewRequest("DELETE", c.getURL("/v1/auth/tokens"), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("X-Auth-token",c.credentials.token)
+	req.Header.Set("X-Subject-token",c.credentials.token)
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("could not delete token")
+	}
+	c.credentials = nil
+
+	return nil
+}
